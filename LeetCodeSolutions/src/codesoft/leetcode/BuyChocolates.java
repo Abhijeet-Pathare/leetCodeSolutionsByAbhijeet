72a00d39f3e227db91b7d4ab25aca2b8ac967bed
Abhijeet Pathare <abhijeetpathare2020@gmail.com>
2023-12-21 12:16:37 +0530

    commit

--- LeetCodeSolutions/src/codesoft/leetcode/BuyChocolates.java
+++ LeetCodeSolutions/src/codesoft/leetcode/BuyChocolates.java
@@ -2,52 +2,62 @@ package codesoft.leetcode;
 
 import java.util.ArrayList;
 import java.util.List;
-/**
- * Leet Code
- * 2706. Buy Two Chocolates
- * Input: prices = [1,2,2], money = 3
-Output: 0
-Explanation: Purchase the chocolates priced at 1 and 2 units respectively. You will have 3 - 3 = 0 units of money afterwards. Thus, we return 0.
-Example 2:
 
-Input: prices = [3,2,3], money = 3
-Output: 3
-Explanation: You cannot buy 2 chocolates without going in debt, so we return 3.
-	
+/**
+ * Leet Code 2706. Buy Two Chocolates You are given an integer array prices
+ * representing the prices of various chocolates in a store. You are also given
+ * a single integer money, which represents your initial amount of money.
+ * 
+ * You must buy exactly two chocolates in such a way that you still have some
+ * non-negative leftover money. You would like to minimize the sum of the prices
+ * of the two chocolates you buy.
+ * 
+ * Return the amount of money you will have leftover after buying the two
+ * chocolates. If there is no way for you to buy two chocolates without ending
+ * up in debt, return money. Note that the leftover must be non-negative.
+ * 
+ * Input:
+ * prices = [1,2,2], money = 3 Output: 0 Explanation: Purchase the chocolates
+ * priced at 1 and 2 units respectively. You will have 3 - 3 = 0 units of money
+ * afterwards. Thus, we return 0. Example 2:
+ * 
+ * Input: prices = [3,2,3], money = 3 Output: 3 Explanation: You cannot buy 2
+ * chocolates without going in debt, so we return 3.
+ * 
  */
 
 public class BuyChocolates {
 	public static int buyChoco(int[] prices, int money) {
-        int i =0;
-        List<Integer> sum = new ArrayList<>();
-        while(i<prices.length-1){
-            int j=i+1;
-            while(j<prices.length){
-                if(prices[i] + prices[j]<=money){
-                    sum.add(prices[i] + prices[j]);
-                }
-                j++;
-            }
-            i++;
-        }
-       
-        int k=1;
-        if(sum.size()>0){
-        int min =sum.get(0);
-        while(k<sum.size()){
-            if(min>sum.get(k)){
-                min = sum.get(k);
-            }
-            k++;
-        }
-        return money - min;
-        }
-        return money;
-      
-    }
-	
+		int i = 0;
+		List<Integer> sum = new ArrayList<>();
+		while (i < prices.length - 1) {
+			int j = i + 1;
+			while (j < prices.length) {
+				if (prices[i] + prices[j] <= money) {
+					sum.add(prices[i] + prices[j]);
+				}
+				j++;
+			}
+			i++;
+		}
+
+		if (sum.size() > 0) {
+			int min = sum.get(0);
+			int k = 1;
+			while (k < sum.size()) {
+				if (min > sum.get(k)) {
+					min = sum.get(k);
+				}
+				k++;
+			}
+			return money - min;
+		}
+		return money;
+
+	}
+
 	public static void main(String[] args) {
-		int[] arr = {69,91,78,19,40,13}; //output should be 62 for this test case
-		System.out.println(buyChoco(arr,94));
+		int[] arr = { 3, 2, 3 };// {69,91,78,19,40,13}; //output should be 62 for this test case
+		System.out.println(buyChoco(arr, 3));
 	}
 }
--- LeetCodeSolutions/src/codesoft/leetcode/MergeStringsAlternatively.java
+++ LeetCodeSolutions/src/codesoft/leetcode/MergeStringsAlternatively.java
@@ -1,57 +1,4 @@
 package codesoft.leetcode;
-
-/**
- * Given a string s, reverse only all the vowels in the string and return it.
-
-The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower and upper cases, more than once.
-
-Example 1:
-
-Input: s = "hello"
-Output: "holle"
-Example 2:
-
-Input: s = "leetcode"
-Output: "leotcede"
- */
-
-public class ReverseVowelsOfAString {
-	public static boolean isVowel(char x){
-        if(x=='a' || x=='e' || x=='i' || x=='o' || x=='u' ||
-           x=='A' || x=='E' || x=='I' || x=='O' || x=='U'){
-               return true;
-           }
-           return false;
-    }
-
-    public static String reverseVowels(String s) {
-        char[] ch = s.toCharArray();
-        int start = 0;
-        int end = s.length() - 1;
-
-        while(start<end ){
-            if(isVowel(s.charAt(start)) && isVowel(s.charAt(end))){
-                char temp = ch[start];
-                ch[start]= ch[end];
-                ch[end] = temp;
-                start++;
-                end--;
-            }else if(isVowel(s.charAt(start))){
-                end--;
-            }else{
-                start++;
-            }
-        }
-        return String.valueOf(ch);
-    }
-    
-    public static void main(String[] args) {
-		String s = "hello";
-		System.out.println(reverseVowels(s));
-	}
-}
-
-package codesoft.leetcode;
 /**
  * @author Abhijeet Pathare
  * Please note: Scroll down to see the question and sample test cases
@@ -120,4 +67,3 @@ Constraints:
 1 <= word1.length, word2.length <= 100
 word1 and word2 consist of lowercase English letters.
  */
-
