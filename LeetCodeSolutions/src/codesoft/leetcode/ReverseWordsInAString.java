57830ca85e203acf4beec60e0665a9908be418f9
Abhijeet Pathare <abhijeetpathare2020@gmail.com>
2023-12-27 11:06:00 +0530

    Reverse words in a String

---
+++ LeetCodeSolutions/src/codesoft/leetcode/ReverseWordsInAString.java
@@ -0,0 +1,56 @@
+package codesoft.leetcode;
+
+public class ReverseWordsInAString {
+	
+	public static void main(String[] args) {
+		String s = "    I am Abhijeet";
+		String str = s.trim();
+		String[] str1 = str.split("\\s+");//this returns an array of strings excluding spaces. i.e words
+		StringBuffer sb = new StringBuffer();//StringBuffer as strings are not mutable
+		for(String s1 : str1) {
+			System.out.println(s1);
+		}
+		for(int i=str1.length-1;i>=0;i--) {
+			sb.append(str1[i]+" ");//append the last word first and also append a space after the word
+		}
+		System.out.println(sb.toString().trim());// used trim to remove the trailing space at the end which will be added after the last word
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// String str = s.trim();
+//String[] str1 = s.split("\\s+");
+//StringBuffer sb = new StringBuffer();
+//for(int i =str1.length-1;i>=0;i--){
+//    sb.append(str1[i]+" ");
+//}
+//return sb.toString().trim();
