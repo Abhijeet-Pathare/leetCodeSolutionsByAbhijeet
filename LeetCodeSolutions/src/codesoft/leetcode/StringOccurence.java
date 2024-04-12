10a377f503073261fe671163b906a3c2a02ad421
Abhijeet Pathare <abhijeetpathare2020@gmail.com>
2024-04-12 16:17:58 +0530

    StringOccurence code added

--- LeetCodeSolutions/src/codesoft/leetcode/StringOccurence.java
+++ LeetCodeSolutions/src/codesoft/leetcode/StringOccurence.java
@@ -1,21 +1,29 @@
-//package codesoft.leetcode;
-//
-//import java.util.Arrays;
-//import java.util.HashMap;
-//import java.util.Map;
-//
-//public class StringOccurence {
-//	
-//	
-//	
-//	public static void main(String[] args) {
-//		String city = "mumbai";
-//		char[] cityArr = city.toCharArray();
-//		Map<Character,Integer> map1 = new HashMap<>();
-//		
-//	}
-//
-//}
+package codesoft.leetcode;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class StringOccurence {
+	
+	
+	
+	public static void main(String[] args) {
+		String city = "mumbai";
+		char[] cityArr = city.toCharArray();
+		Map<Character,Integer> map1 = new HashMap<>();
+		for(int i=0;i<cityArr.length;i++) {
+			if(map1.containsKey(cityArr[i])) {
+				map1.put(cityArr[i], map1.get(cityArr[i])+1);
+			}else {
+				map1.put(cityArr[i], 1);
+			}
+		}
+		System.out.println(map1);
+		
+	}
+
+}
 
 
 /*
