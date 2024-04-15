0707fbf1367c681e2a537f265bb242f4660e2f26
Abhijeet Pathare <abhijeetpathare2020@gmail.com>
2024-04-15 14:18:09 +0530

    Majority elem using moores voting algo

--- LeetCodeSolutions/src/codesoft/leetcode/MajorityElement.java
+++ LeetCodeSolutions/src/codesoft/leetcode/MajorityElement.java
@@ -5,6 +5,35 @@ import java.util.Map;
 
 public class MajorityElement {
 
+	//solution using moores voting algorithm
+	public static int mooresSolution(int[] arr) {
+		int vote = 1;
+		int candidate = arr[0];
+		for(int i=1;i<arr.length;i++) {
+			if(arr[i-1]==arr[i]) {
+				vote++;
+			}else {
+				vote--;
+			}
+			if(vote==0) {
+				candidate = arr[i];
+			}
+		}
+		//verify if candidate is the answer or not
+		int counter = 0;
+		for(int i=0;i<arr.length;i++){
+			if(candidate==arr[i]) {
+				counter++;
+			}
+			if(counter >arr.length/2) {
+				return candidate;
+			}
+		}
+		return -1;
+	}
+	
+	
+	//solution using hashmap approach
 	public static int solution(int[] arr) {
 		int length = arr.length;
 		Map<Integer,Integer> map = new HashMap<>();
@@ -25,7 +54,8 @@ public class MajorityElement {
 	}
 	public static void main(String[] args) {
 		int[] arr = {2,2,6,6,6,2,2,8,2,2};
-		int ans = solution(arr);
+		//int ans = solution(arr);
+		int ans = mooresSolution(arr);
 		System.out.println("Majority element is "+ans);
 	}
 	
